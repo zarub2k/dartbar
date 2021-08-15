b535e77555763df19337e54f9da9e15e637f4c6b
Tham <zarub2k@gmail.com>
2021-08-15 19:27:33 +0530

    List example tried

---
+++ bin/basic/lists.dart
@@ -0,0 +1,51 @@
+void main(List<String> arugments) {
+  forList();
+}
+
+void forList() {
+  var values = [1, 2, 3];
+  var newValues = [
+    ...values,
+    for(var i in values) i * 2
+  ];
+  print(newValues);
+}
+
+void ifList() {
+  var values = [1, 2];
+  var newValues = [...values, if(values.length > 2) 0];
+  print(newValues);
+}
+
+void spreadList() {
+  var values = [1, 2, 3];
+  var latestValues = [5, ...values, 6];
+  print(latestValues);
+
+  //null aware
+  var initials;
+  var names = ['Tham', 'Roger', ...?initials];
+  print(names);
+}
+
+void constList() {
+  var values = const [1, 2, 3];
+  print(values);
+}
+
+void simpleList() {
+  var names = ['Tham', 'Roger', 'Joko'];
+  printList(names);
+
+  names.add('Nadal');
+  names[0] = 'Feddy';
+  printList(names);
+}
+
+void printList(List<String> names) {
+  print('First element: ${names[0]}');
+  names.forEach((name) {
+    print(name);
+  });
+  print(names.length);
+}
