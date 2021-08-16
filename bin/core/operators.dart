84e25b5037c15632aa0a9d9271096f5fa35ac2c6
Tham <zarub2k@gmail.com>
2021-08-16 22:13:04 +0530

    arithmetic operator tried

--- bin/core/operators.dart
+++ bin/core/operators.dart
@@ -1,3 +1,14 @@
 void main(List<String> args) {
   print('### Operators ###');
+  arithmeticOp(6, 3);
+}
+
+void arithmeticOp(int a, int b) {
+  print('$a + $b = ${a + b}');
+  print('$a - $b = ${a - b}');
+  print('$a * $b = ${a * b}');
+  print('$a / $b = ${a / b}');
+  print('-($a + $b) = -${a + b}');
+  print('$a ~/ $b = ${a ~/ b}');
+  print('$a % $b = ${a % b}');
 }
