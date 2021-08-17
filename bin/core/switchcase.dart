b6c6fc33facb565c88ea3d0e364b045edebeb8b3
Tham <zarub2k@gmail.com>
2021-08-17 23:25:56 +0530

    switch case started

---
+++ bin/core/switchcase.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  print('### Switch and case ###');
+}
+
+void switchCase() {
+  var players = {
+    'Joko': 'Male',
+    'Roger': 'Male',
+    'Serena': 'Female'
+  };
+
+  switch (players) {
+
+  }
+}
