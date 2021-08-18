45a51cb1191bc3b7aeb548ab6d9ae51916b4e23c
Tham <zarub2k@gmail.com>
2021-08-18 07:57:33 +0530

    switch case continues

--- bin/core/switchcase.dart
+++ bin/core/switchcase.dart
@@ -1,15 +1,35 @@
 void main(List<String> args) {
   print('### Switch and case ###');
+  fallthroughCase();
+}
+
+void fallthroughCase() {
+  var commands = ['ON', 'OFF', 'PAUSE'];
+  for (var command in commands) {
+    switch(command) {
+      case 'PAUSE':
+      case 'ON':
+        print('Execute \'ON\' for $command');
+        break;
+
+      case 'OFF':
+        print('Execute \'OFF\' for $command');
+        break;
+    }
+  }
 }
 
 void switchCase() {
-  var players = {
-    'Joko': 'Male',
-    'Roger': 'Male',
-    'Serena': 'Female'
-  };
+  var commands = ['ON', 'OFF', 'PAUSE'];
+  for (var command in commands) {
+    switch(command) {
+      case 'ON':
+        print('Execute \'ON\' command');
+        break;
 
-  switch (players) {
-
+      case 'OFF':
+        print('Execute \'OFF\' command');
+        break;
+    }
   }
 }
