201d3dd46388bb4e55d7b761d2e0ba6553010b39
Tham <zarub2k@gmail.com>
2021-08-18 08:04:22 +0530

    switch case completed

--- bin/core/switchcase.dart
+++ bin/core/switchcase.dart
@@ -1,6 +1,25 @@
 void main(List<String> args) {
   print('### Switch and case ###');
-  fallthroughCase();
+  fallthroughWithDelegate();
+}
+
+void fallthroughWithDelegate() {
+  var commands = ['ON', 'OFF', 'PAUSE'];
+  for (var command in commands) {
+    switch(command) {
+      case 'PAUSE':
+        print('Execute \'PAUSE\' for $command');
+        continue postPause;
+      case 'ON':
+        print('Execute \'ON\' for $command');
+        break;
+
+      postPause:
+      case 'OFF':
+        print('Execute \'OFF\' for $command');
+        break;
+    }
+  }
 }
 
 void fallthroughCase() {
