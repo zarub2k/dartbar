d9d7a1b30cb3b2d269ec957aaf30d18b23df2ae3
Tham <zarub2k@gmail.com>
2021-08-20 15:22:00 +0530

    Exception work started

--- bin/core/exceptions.dart
+++ bin/core/exceptions.dart
@@ -1,8 +1,54 @@
 void main(List<String> args) {
   print('### Exception examples ###');
-  print(divide(1, 0));
+  tryCatchOn();
+}
+
+void tryCatchOn() {
+  try {
+    exception();
+  } on FormatException catch (e) {
+    print('tryCatchOn()');
+    print(e);
+  }
+}
+
+void tryCatch() {
+  try {
+    exception();
+  } catch (e) {
+    print(e);
+  }
+}
+
+void simpleTry() {
+  try {
+    exception();
+  } on FormatException {
+    print('Inside format exception');
+  }
+}
+
+void exception() {
+  throw FormatException('Formatting is not correct');
+}
+
+void anythingAsEx() {
+  throw 'Throwing any object';
 }
 
 double divide(int a, int b) {
-  return a / b;
+  var result;
+  try {
+    result = a / b;
+    if (result == double.infinity) {
+      throw Exception('Number can\'t be divided by zero');
+    }
+  } on Exception catch (e) {
+    print('Exception is caught');
+    print(e.toString());
+  } catch (e) {
+    print('Inside general catch');
+  }
+
+  return result;
 }
