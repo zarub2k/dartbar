f534f5d270f12eaee08da33f04b2e974d6b3c5af
Tham <zarub2k@gmail.com>
2021-08-20 22:54:16 +0530

    Built-in types tried

---
+++ bin/core/builtin_types.dart
@@ -0,0 +1,41 @@
+void main(List<String> args) {
+  print('### Built-in Types ###');
+
+  // numbers();
+  // strings();
+  boolean();
+}
+
+void numbers() {
+  var count = 0;
+  print('++Count ${++count}');
+
+  var result = 1 / 2;
+  print(result);
+
+  num a = 1;
+  print(a);
+}
+
+void strings() {
+  var name = 'Roger';
+  print('Name here is $name');
+
+  var multiline = '''
+    This is a long text. This has multiple
+    lines and we can have any number of lines.
+  ''';
+  print(multiline);
+}
+
+void boolean() {
+  var canMove = false;
+  print('Can move: $canMove');
+  print('Opposite of canMove: ${!canMove}');
+
+  if (canMove) {
+    print('Given value is true');
+  } else {
+    print('Given value is false');
+  }
+}
