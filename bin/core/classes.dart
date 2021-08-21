86644a069a65d39f9c5ec287eb7cc265085a316e
Tham <zarub2k@gmail.com>
2021-08-21 22:11:04 +0530

    My first class is created

---
+++ bin/core/classes.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  print('### Classes example ###');
+  var calculator = Calculator();
+  print(calculator.add(4, 5));
+}
+
+class Calculator {
+  int add(int a, int b) {
+    return a + b;
+  }
+
+  int subtract(int a, int b) {
+    return a - b;
+  }
+
+  int multiply(int a, int b) {
+    return a * b;
+  }
+
+  double divide(int a, int b) {
+    return a / b;
+  }
+}
