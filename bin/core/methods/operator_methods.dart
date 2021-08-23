7c6bcb78cf5eae45d8837299f195a46dec433243
Tham <zarub2k@gmail.com>
2021-08-23 22:40:16 +0530

    Instance method work started

--- bin/core/methods/operator_methods.dart
+++ bin/core/methods/operator_methods.dart
@@ -1,3 +1,11 @@
 void main(List<String> args) {
   print('### Operator methods ###');
 }
+
+class Vector {
+  final int x, y;
+  Vector(this.x, this.y);
+
+  Vector operator + (Vector v) => Vector(x + v.x, y + v.y);
+  Vector operator - (Vector v) => Vector(x - v.x, y - v.y);
+}
