0e42d42ca035eccc90cc7213a0ab0e9a9c0688da
Tham <zarub2k@gmail.com>
2021-08-26 15:35:49 +0530

    unit testing framework is integrated

--- bin/tests/helloword.dart
+++ bin/tests/helloword.dart
@@ -4,13 +4,23 @@ void main(List<String> args) {
   
   test('Simple hello world test', () {
     var name = 'Tham';
-    expect('Tham', equals(name));
+    expect(name, equals('Tham'));
   });
 
   test('Validate the size of a list', () {
     var values = [1, 2, 3];
-    expect(2, equals(values.length),
+    expect(values.length, equals(3),
       reason: 'Length is not matched'
     );
+
+    expect(values[1], equals(2));
+  });
+
+  group('List:', () {
+    test('description', () {
+      var values = [1, 2, 3];
+      values.add(4);
+      expect(values, equals([1, 2, 3, 4]));
+    });
   });
 }
