997df1933e6e7a4cf0b9647669f7a4e2728337d6
Tham <zarub2k@gmail.com>
2021-09-06 19:38:50 +0530

    explicit extn method called

--- bin/core/classes/extn_methods.dart
+++ bin/core/classes/extn_methods.dart
@@ -2,8 +2,13 @@
 
 void main(List<String> args) {
   print('### Extension methods');
+
+  //calling extension method
   print('5'.parseInt());
   print('Tham'.sayHello());
+
+  //applying extension explicitly
+  print(StringExt('55').sayHello());
 }
 
 extension StringExt on String {
