79ce1d5accefccdf2fb62abfe7bfe0090a62f11f
Tham <zarub2k@gmail.com>
2021-09-06 09:43:13 +0530

    extension method is written

--- bin/core/classes/extn_methods.dart
+++ bin/core/classes/extn_methods.dart
@@ -1,10 +1,15 @@
 void main(List<String> args) {
   print('### Extension methods');
   print('5'.parseInt());
+  print('Tham'.sayHello());
 }
 
 extension StringExt on String {
   int parseInt() {
     return int.parse(this);
+  }
+
+  String sayHello() {
+    return 'Hello $this';
   }
 }
