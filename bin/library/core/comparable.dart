246cdfe5cf8292c39b9e788ddee5b045b0c2fe23
Tham <zarub2k@gmail.com>
2021-09-14 07:57:05 +0530

    sort function tried

--- bin/library/core/comparable.dart
+++ bin/library/core/comparable.dart
@@ -5,6 +5,14 @@ void main(List<String> args) {
   var line2 = Line(11);
   var diff = line1.compareTo(line2);
   print((diff < 0) ? 'Line1 is shorter' : 'Line2 is shorter');
+
+  var lines = [Line(40), Line(25), Line(50), Line(20)];
+  lines.sort();
+  print(lines);
+
+  var people = [Person('Tham'), Person('Roger'), Person('Nadal')];
+  people.sort((p1, p2) => p1.name.compareTo(p2.name));
+  print(people);
 }
 
 class Line implements Comparable<Line> {
@@ -14,5 +22,20 @@ class Line implements Comparable<Line> {
   @override
   int compareTo(Line other) {
     return length - other.length;
+  }
+
+  @override
+  String toString() {
+    return 'Line{length: $length}';
+  }
+}
+
+class Person {
+  String name;
+  Person(this.name);
+
+  @override
+  String toString() {
+    return 'Person($name)';
   }
 }
